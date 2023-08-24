b399f88a3d01a96eb5da0a109ca686803ad01cdc
rakesh shetty <rakeshshetty8291@gmail.com>
2023-08-24 14:56:00 +0530

    new commit

---
+++ src/customer/Components/Carousel/HomeCaroselData.js
@@ -0,0 +1,20 @@
+export const homeCarouselData=[
+    {
+        image:"card1.jpg",
+        path:"/animal/pet/dog"
+    },
+    {
+        image:"card2.jpg",
+        path:"/animal/pet/cat"
+    },
+    {
+        image:"card3.jpg",
+        path:"/animal/pet/rabbits"
+    },
+    {
+        image:"card4.jpg",
+        path:"/women/clothing/women_saree"
+    }
+
+]
+
